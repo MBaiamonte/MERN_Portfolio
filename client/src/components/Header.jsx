bad6a29f2793148ba8f3cad6375c60a13a6b121f
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-02-25 15:30:34 -0700

    header update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,32 +4,32 @@ import {LinkContainer} from 'react-router-bootstrap';
 
 const Header = () => {
     return (
-            <Navbar expand='md' fixed="top">
-                <Container>
-                    <LinkContainer to="/" >
-                        <Navbar.Brand>
-                            <h2>Matthew Baiamonte</h2>
-                        </Navbar.Brand>
-                    </LinkContainer>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                    <Navbar.Collapse id="responsive-navbar-nav">   
-                        <Nav className='ms-auto justify-content-around w-100'>
-                            <LinkContainer to='/Projects'>
-                                <Nav.Link >Projects</Nav.Link>
-                            </LinkContainer>
-                            <LinkContainer to='/Work_History'>
-                                <Nav.Link >Work History</Nav.Link>
-                            </LinkContainer>
-                            <LinkContainer to='/Education'>
-                                <Nav.Link >Education</Nav.Link>
-                            </LinkContainer>
-                            <LinkContainer to='/About_Me'>
-                                <Nav.Link >About Me</Nav.Link>
-                            </LinkContainer>
-                        </Nav>
-                        </Navbar.Collapse>
+                <Container className='mb-3 '>
+                    <Navbar expand='md' fixed="top">
+                        <LinkContainer to="/" >
+                            <Navbar.Brand>
+                                <h2>Matthew Baiamonte</h2>
+                            </Navbar.Brand>
+                        </LinkContainer>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                        <Navbar.Collapse id="responsive-navbar-nav">   
+                            <Nav className='ms-auto justify-content-around w-100'>
+                                <LinkContainer to='/Projects'>
+                                    <Nav.Link >Projects</Nav.Link>
+                                </LinkContainer>
+                                <LinkContainer to='/Work_History'>
+                                    <Nav.Link >Work History</Nav.Link>
+                                </LinkContainer>
+                                <LinkContainer to='/Education'>
+                                    <Nav.Link >Education</Nav.Link>
+                                </LinkContainer>
+                                <LinkContainer to='/About_Me'>
+                                    <Nav.Link >About Me</Nav.Link>
+                                </LinkContainer>
+                            </Nav>
+                            </Navbar.Collapse>
+                        </Navbar>
                     </Container>
-            </Navbar>
     )
 }
 
