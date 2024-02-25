53ea0ba28e393ccc38b85266a4097f25b20bc2e8
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-02-25 14:54:05 -0700

    add landingPage comp, linked it to App.jsx. add header to landingPage comp

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
+import React from "react";
+import LandingPage from "./views/LandingPage";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-      hello world
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<LandingPage/>}  path="/"/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import {LinkContainer} from 'react-router-bootstrap';
 
@@ -6,28 +6,28 @@ const Header = () => {
     return (
             <Navbar expand='md' fixed="top">
                 <Container>
-                <LinkContainer to="/" >
-                    <Navbar.Brand>
-                        <h2>Matthew Baiamonte</h2>
-                    </Navbar.Brand>
-                </LinkContainer>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
-                <Navbar.Collapse id="responsive-navbar-nav">   
-                    <Nav className='ms-auto justify-content-around w-100'>
-                        <LinkContainer to='/Projects'>
-                            <Nav.Link >Projects</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to='/Work_History'>
-                            <Nav.Link >Work History</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to='/Education'>
-                            <Nav.Link >Education</Nav.Link>
-                        </LinkContainer>
-                        <LinkContainer to='/About_Me'>
-                            <Nav.Link >About Me</Nav.Link>
-                        </LinkContainer>
-                    </Nav>
-                    </Navbar.Collapse>
+                    <LinkContainer to="/" >
+                        <Navbar.Brand>
+                            <h2>Matthew Baiamonte</h2>
+                        </Navbar.Brand>
+                    </LinkContainer>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                    <Navbar.Collapse id="responsive-navbar-nav">   
+                        <Nav className='ms-auto justify-content-around w-100'>
+                            <LinkContainer to='/Projects'>
+                                <Nav.Link >Projects</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/Work_History'>
+                                <Nav.Link >Work History</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/Education'>
+                                <Nav.Link >Education</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/About_Me'>
+                                <Nav.Link >About Me</Nav.Link>
+                            </LinkContainer>
+                        </Nav>
+                        </Navbar.Collapse>
                     </Container>
             </Navbar>
     )
