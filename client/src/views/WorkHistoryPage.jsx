321d0954b1c0fb415f841d918cc5773daf079fd9
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-02-27 16:29:41 -0700

    create work history view page comp

---
+++ client/src/views/WorkHistoryPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import WorkHistory from '../components/WorkHistory';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+
+const WorkHistoryPage = () => {
+    return (
+        <>
+        <Header/>
+        <WorkHistory/>
+        <Footer/>
+        </>
+    )
+}
+
+export default WorkHistoryPage
