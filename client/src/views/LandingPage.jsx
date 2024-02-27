18787b58b6522397b12aa40ccf9b3318c7c60e41
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-02-27 16:25:50 -0700

    add work history comp

--- client/src/views/LandingPage.jsx
+++ client/src/views/LandingPage.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from '../components/Header';
 import EducationQuickView from '../components/EducationQuickView';
 import Footer from '../components/Footer';
+import WorkHistory from '../components/WorkHistory';
 
 const LandingPage = () => {
     return (
         <>
             <Header/>
             <EducationQuickView/>
+            <WorkHistory/>
             <Footer/>
         </>
     )
