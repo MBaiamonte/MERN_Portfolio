4a654cf734e25c42d31d4baa1ab73c0e6870f278
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-02-27 17:00:19 -0700

    update card for flip animation

--- client/src/components/EducationQuickView.jsx
+++ client/src/components/EducationQuickView.jsx
@@ -1,61 +1,67 @@
-import React from 'react';
-import { Container, Card, Row , Col, Button} from 'react-bootstrap';
-import { LinkContainer} from 'react-router-bootstrap';
-import {Link} from 'react-router-dom'
+import React, {useState} from 'react';
+// import { Container, Card, Row , Col, Button} from 'react-bootstrap';
+
+// import {Link} from 'react-router-dom'
 
 const EducationQuickView = () => {
+        //Declarations 
+        const [flip1, setFlip1] = useState(false)
+        const [flip2, setFlip2] = useState(false)
+        const [flip3, setFlip3] = useState(false)
     return (
-        <Container className='pt-5'>
-            <Row>
-                <Col >
-                    <Card>
-                        <Card.Body>
-                        <Row className='d-flex justify-content-center align-items-center mb-3'>
-                            <Card.Img variant="top" style={{'maxHeight':'75px', 'height': '70%' ,'width':'auto'}} src="../images/UAF_Banner.png" />
-                        </Row> 
-                        <Row className='d-flex justify-content-center flex-column align-items-center'> 
-                            <Card.Title>University of Alaska Fairbanks</Card.Title>
-                            <Card.Subtitle>Bachelors in Biology</Card.Subtitle>
-                            <Card.Text>August 2019 - May 2021</Card.Text>
-                        </Row>
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col>
-                <Card>
-                    <Card.Body>
-                        <Row className='d-flex justify-content-center align-items-center'>
-                            <Card.Img variant="top" style={{'maxHeight':'75px', 'height': '80%' ,'width':'auto'}} src="../images/Coding_Dojo_Bootcamp_Banner.png" />
-                        </Row>
-                        <Row className='d-flex justify-content-center flex-column align-items-center'> 
-                            <Card.Title className='mt-3'>Coding Dojo Bootcamp</Card.Title>
-                            <Card.Subtitle>Full-Stack Web Development</Card.Subtitle>
-                            <Card.Subtitle className='mt-1'>MERN, Java, and Python</Card.Subtitle>
-                            <Card.Text>Dec 2022 - Oct 2023</Card.Text>
-                        </Row>
-                    </Card.Body>
-                </Card>
-                </Col>
-                <Col>
-                    <Card >
-                        <Card.Body>
-                            <Row className='d-flex justify-content-center alight-items-center mb-3'>
-                                <Card.Img variant="top" style={{'maxHeight':'75px', 'height': '80%' ,'width':'auto'}} src="../images/CU_Boulder_Banner_2.png" />
-                            </Row>
-                            <Row className='d-flex justify-content-center flex-column align-items-center'> 
-                                <Card.Title>University Colorado Boulder</Card.Title>
-                                <Card.Subtitle>Studied Integrated Physiology</Card.Subtitle>
-                                <Card.Text>August 2016 - May 2018</Card.Text>
-                            </Row>
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-            <Row className='d-flex justify-content-center align-items-center mt-4'>
-                <Link to={`/Education`}>Check out in detail</Link>
-            </Row>
+        <>
 
-        </Container>
+        <div className='card-grid my-5'>
+        <div className={`card education ${(flip1 ? "flip" : "")}`}>
+            <div className='front' onClick={()=> setFlip1(!flip1)}>
+                <div>
+                    <img src="../images/UAF_Banner.png" className='my-2' style={{'maxHeight':'75px', 'height': '30%' ,'maxWidth':'100%'}} />
+                </div>
+                <h4>University of Alaska Fairbanks</h4>
+                <h5>Bachelors in Biology</h5>
+                <p>August 2019 - May 2021</p>
+                <p>Boulder, CO</p>
+            </div>
+            <div className='back' onClick={()=> setFlip1(!flip1)}>
+                <h4>GPA</h4>
+                <p>back side</p>
+            </div>
+        </div>
+
+        <div className={`card education ${(flip2 ? "flip" : "")}`}>
+            <div className='front' onClick={()=> setFlip2(!flip2)}>
+                <div>
+                    <img src='../images/Coding_Dojo_Bootcamp_Banner.png' className='my-2' style={{'maxHeight':'85px', 'height': '40%' ,'maxWidth':'100%'}} />
+                </div>
+                <h4>Coding Dojo Bootcamp</h4>
+                <h5>Full-Stack Web Development</h5>
+                <h6>MERN, Java, and Python</h6>
+                <p>Dec 2022 - Oct 2023</p>
+                <p>Remote</p>
+            </div>
+            <div className='back' onClick={()=> setFlip2(!flip2)}>
+                <h4>Job Duties</h4>
+                <p>back side</p>
+            </div>
+        </div>
+
+        <div className={`card education ${(flip3 ? "flip" : "")}`}>
+            <div className='front' onClick={()=> setFlip3(!flip3)}>
+                <div>
+                    <img src='../images/CU_Boulder_Banner_2.png' className='my-2' style={{'maxHeight':'85px', 'height': '40%' ,'maxWidth':'100%'}} />
+                </div>
+                <h4>University Colorado Boulder</h4>
+                <h5>Studied Integrated Physiology</h5>
+                <p>August 2016 - May 2018</p>
+                <p>Boulder, CO</p>
+            </div>
+            <div className='back' onClick={()=> setFlip3(!flip3)}>
+                <h4>Job Duties</h4>
+                <p>back side</p>
+            </div>
+        </div>
+    </div>
+    </>
     )
 }
 
