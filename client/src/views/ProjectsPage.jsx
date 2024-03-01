1e0ee4eed739c75843729a611fac0f6f2f990f67
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-03-01 15:33:30 -0700

    create projects page

---
+++ client/src/views/ProjectsPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Projects from '../components/Projects'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+import Separator from '../components/Separator'
+
+const ProjectsPage = () => {
+    return (
+        <>
+            <Header/>
+            <Separator/>
+            <Projects/>
+            <Footer/>
+        </>
+    )
+}
+
+export default ProjectsPage
