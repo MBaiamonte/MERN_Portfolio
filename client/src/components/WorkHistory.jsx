0f8c2dba9d82b3cbea1436a1a8a1e11345075861
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-02-29 17:27:30 -0700

    update work history

--- client/src/components/WorkHistory.jsx
+++ client/src/components/WorkHistory.jsx
@@ -1,12 +1,19 @@
-import React, {useState} from 'react'
-import '../index.css'
+import React, {useState} from 'react';
+import '../index.css';
+import {useNavigate} from 'react-router-dom';
 
 
 const WorkHistory = () => {
     //Declarations 
-    const [flip1, setFlip1] = useState(false)
-    const [flip2, setFlip2] = useState(false)
-    const [flip3, setFlip3] = useState(false)
+    const [flip1, setFlip1] = useState(false);
+    const [flip2, setFlip2] = useState(false);
+    const [flip3, setFlip3] = useState(false);
+    const navigate = useNavigate();
+
+    //Functions
+    const workNavigation = () =>{
+        navigate('/Work_History')
+    }
 
     return (
         <div className='d-flex flex-column justify-content-center '>
@@ -21,10 +28,20 @@ const WorkHistory = () => {
                         <h5>Research Associate</h5>
                         <p>Feb/2022 - Dec/2022</p>
                         <p>Boulder, CO</p>
+                        <div className='mt-2'>
+                            <img src="../images/flip_card_icon.png" alt="flip icon" style={{'maxHeight':'25px', 'height': '30%' ,'maxWidth':'100%'}}/>
+                        </div>
                     </div>
                     <div className='back' onClick={()=> setFlip1(!flip1)}>
-                        <h4>Job Duties</h4>
-                        <p>back side</p>
+                        <div>
+                            <img src="../images/histology_clip_art.png" alt="flip icon" style={{'maxHeight':'95px', 'height': '30%' ,'maxWidth':'100%'}}/>
+                        </div>
+                        <h4>Inotiv Bioanalytical Systems</h4>
+                        <h5>Research Associate | Histologist </h5>
+                        <button onClick={workNavigation}>View in Detail</button>
+                        <div>
+                            <img src="../images/flip_card_icon.png" alt="flip icon" className='mt-3' style={{'maxHeight':'25px', 'height': '30%' ,'maxWidth':'100%'}}/>
+                        </div>
                     </div>
                 </div>
                 <div className={`card ${(flip2 ? "flip" : "")}`}>
@@ -36,10 +53,20 @@ const WorkHistory = () => {
                         <h5>Microbiologist</h5>
                         <p>May/2021 - Feb/2022</p>
                         <p>Greeley, CO</p>
+                        <div className='mt-2'>
+                            <img src="../images/flip_card_icon.png" alt="flip icon" style={{'maxHeight':'25px', 'height': '30%' ,'maxWidth':'100%'}}/>
+                        </div>
                     </div>
                     <div className='back' onClick={()=> setFlip2(!flip2)}>
-                        <h4>Job Duties</h4>
-                        <p>back side</p>
+                        <div>
+                            <img src="../images/microbiologist_clip_art.png" alt="flip icon" style={{'maxHeight':'95px', 'height': '30%' ,'maxWidth':'100%'}}/>
+                        </div>
+                        <h4>IEH Labs & Consulting</h4>
+                        <h5>Microbiologist</h5>
+                        <button onClick={workNavigation}>View in Detail</button>
+                        <div>
+                            <img src="../images/flip_card_icon.png" alt="flip icon"  className='mt-3' style={{'maxHeight':'25px', 'height': '30%' ,'maxWidth':'100%'}}/>
+                        </div>
                     </div>
                 </div>
                 <div className={`card ${(flip3 ? "flip" : "")}`}>
@@ -51,10 +78,20 @@ const WorkHistory = () => {
                         <h5>Whitewater Raft Guide</h5>
                         <p>dates</p>
                         <p>Hartford, TN</p>
+                        <div className='mt-2'>
+                            <img src="../images/flip_card_icon.png" alt="flip icon"  style={{'maxHeight':'25px', 'height': '30%' ,'maxWidth':'100%'}}/>
+                        </div>
                     </div>
                     <div className='back' onClick={()=> setFlip3(!flip3)}>
-                        <h4>Job Duties</h4>
-                        <p>back side</p>
+                        <div>
+                            <img src="../images/rafting_clipart.png" alt="flip icon" style={{'maxHeight':'85px', 'height': '30%' ,'maxWidth':'100%'}}/>
+                        </div>
+                        <h4>Smokey Mountain Outdoors</h4>
+                        <h5>Whitewater Raft Guide</h5>
+                        <button onClick={workNavigation}>View in Detail</button>
+                        <div>
+                            <img src="../images/flip_card_icon.png" alt="flip icon" className='mt-3' style={{'maxHeight':'25px', 'height': '30%' ,'maxWidth':'100%'}}/>
+                        </div>
                     </div>
                 </div>
             </div>
--- client/src/views/WorkHistoryPage.jsx
+++ client/src/views/WorkHistoryPage.jsx
@@ -1,18 +1,77 @@
 import React from 'react';
-import WorkHistory from '../components/WorkHistory';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-import Separator from '../components/Separator'
-
+import Separator from '../components/Separator';
+import Slider from "react-slick";
+import '../index.css'
 
 const WorkHistoryPage = () => {
+    
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        pauseOnHover: true
+    };
     return (
-        <>
-        <Header/>
-        <Separator/>
-        <WorkHistory/>
-        <Footer/>
-        </>
+        <div className='d-flex flex-column align-items-center'>
+            <Header/>
+            <Separator/>
+            <h4>Work History</h4>
+            <div className='mb-4' style={{'width': '70%', 'backgroundColor': 'lavenderblush'}}>
+                <Slider {...settings} >
+                <div className='slide'>
+                        <div className='justify-content-center d-flex align-items-center'>
+                            <img src='../images/Inotiv_Logo.png' className='my-2' style={{'maxHeight':'150px', 'height': '40%' ,'width':'auto'}} />
+                        </div>
+                        <div className='d-flex justify-content-between mt-4 mx-4 '>
+                            <div style={{'width':'30%'}} className=' d-flex flex-column align-items-center'>
+                                <h4 style={{'overflowWrap': 'break-word'}}>Inotiv Bioanalytical Systems</h4>
+                                <h5>Research Associate</h5>
+                                <p className='mb-0'>02/2022 - 12/2022</p>
+                                <p>Boulder, CO</p>
+                            </div>
+                            <div style={{'width':'65%'}}>
+                                <h5>Duties</h5>
+                                <li> Created and implemented new processes and SOPs within the Histology, Logistics, Information, and Immuno-Histochemistry departments. Resulted in optimized workflows and improved efficiency. </li>
+                                <li>Maintained and updated all Safety Data Sheet (SDS) documentation for chemicals and substances, demonstrating meticulous attention to detail and compliance with safety regulations. </li>
+                                <li>Effectively managed project workflows through different departments and aided other departments as needed. </li>
+                                <li>Maintained project workflow between departments and aided the lab manager as needed.</li>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='slide'>
+                        <div className='justify-content-center d-flex align-items-center'>
+                            <img src='../images/IEH_Logo.png' className='my-2' style={{'maxHeight':'150px', 'height': '40%' ,'width':'auto'}} />
+                        </div>
+                        <div className='d-flex justify-content-between mt-4 mx-4 '>
+                            <div style={{'width':'30%'}} className=' d-flex flex-column align-items-center'>
+                                <h4 style={{'overflowWrap': 'break-word'}}>IEH Labs & Consulting</h4>
+                                <h5>Microbiologist</h5>
+                                <p className='mb-0'>May/2021 - Feb/2022</p>
+                                <p>Greeley, CO</p>
+                                
+                            </div>
+                            <div style={{'width':'65%'}}>
+                                <h5>Duties</h5>
+                                <li>Processed food samples and amplified bacterial content through incubation processes with thorough workflow management.</li>
+                                <li>Conducted PCR to determine the presence of pathogenic bacterial strains. </li>
+                                <li>Demonstrated expertise in plating and counting bacterial cultures on agar plates. </li>
+                                <li>Maintained project workflow between departments and aided the chief microbiologist as needed.</li>
+                            </div>
+                        </div>
+                    </div>
+                    <div>
+                        <h3>3</h3>
+                    </div>
+                </Slider>
+            </div>
+            <Footer/>
+        </div>
     )
 }
 
