d1ee8485961e2c868f4948b2827900ac14d74057
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-03-03 13:39:11 -0700

    add project content, links and formatting

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -19,12 +19,15 @@ const Projects = () => {
                     icon={<img src='../images/React_Icon.png' style={{'width': 'auto', 'height':'100%'}}/>}
                 >
                     <h3 className="vertical-timeline-element-title">Portfolio</h3>
-                    <h4 className="vertical-timeline-element-subtitle">React</h4>
-                    <p>
-                        dec dec dec dec
-                    </p>
-                    <button>Git</button>
-                    <button>Deployed Link</button>
+                    <h6 className="vertical-timeline-element-subtitle mb-2">React</h6>
+                    <div>
+                        <li> Developed customized UI & UX through CSS stylesheet and Bootstrap library to seamless display content for users </li>
+                        <li> Incorporated react libraries such as React-Slick to enable interactive content display for users </li>
+                        <li> Designed components to allow for simple reusability on any page </li>
+                    </div>
+                    <div className='mt-2'>
+                        <button><a href="https://github.com/MBaiamonte/MERN_Portfolio">Git</a></button>
+                    </div>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--work"
@@ -33,10 +36,16 @@ const Projects = () => {
                     icon={<img src='../images/React_Icon.png' style={{'width': 'auto', 'height':'100%'}}/>}
                 >
                     <h3 className="vertical-timeline-element-title">Barber Shop Booking App</h3>
-                    <h4 className="vertical-timeline-element-subtitle">React,Express, MongoDB Atlas, Node.js</h4>
-                    <p>
-                        dec dec dec
-                    </p>
+                    <h6 className="vertical-timeline-element-subtitle mb-2">React,Express, MongoDB Atlas, Node.js</h6>
+                    <div>
+                        <li>Developed custom error codes and validations for seamless debugging and consistent data entry storage</li>
+                        <li>Implemented Google Maps API for user-friendly display of store location and directions. </li>
+                        <li>Utilized custom CSS and Bootstrap for a clean and intuitive user interface, resulting in an enhanced user experience </li>
+                    </div>
+                    <div className='mt-2'>
+                        <button><a href="https://github.com/MBaiamonte/MERN_Barber_Shop">Git</a></button>
+                        {/* <button>Deployed Link</button> */}
+                    </div>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--work"
@@ -45,10 +54,17 @@ const Projects = () => {
                     icon={<img src='../images/React_Icon.png' style={{'width': 'auto', 'height':'100%'}}/>}
                     >
                     <h3 className="vertical-timeline-element-title">Ecommerce Mock Store Demo</h3>
-                    <h4 className="vertical-timeline-element-subtitle">React, Express, MongoDB Atlas, Node.js, Redux</h4>
-                    <p>
-                        dec dec dec
-                    </p>
+                    <h6 className="vertical-timeline-element-subtitle mb-2">React, Express, MongoDB Atlas, Node.js, Redux</h6>
+                    <div>
+                        <li>Developed admin-protected routes and functions to securely and easily manage users, products, and orders</li>
+                        <li>Incorporated Redux for easy frontend state management and troubleshooting</li>
+                        <li>Enabled PayPal and debit card payments for simplified user checkout</li>
+                        <li>Utilized custom bootstrap style sheet for a clean and intuitive user interface, resulting in an enhanced user experience </li>
+                    </div>
+                    <div className='mt-2'>
+                        <button className='mr-3'><a href="https://github.com/MBaiamonte/MERN_Ecom_Store_Demo">Git</a></button>
+                        <button><a href="https://mern-ecommerce-mock-store.onrender.com/">Deployed Link</a></button>
+                    </div>
                 </VerticalTimelineElement>
                 <VerticalTimelineElement
                     className="vertical-timeline-element--work"
@@ -57,10 +73,12 @@ const Projects = () => {
                     icon={<img src='../images/css_html_js_icon.png' style={{'width': '100%', 'height':'auto'}} className='mt-2'/>}
                     >
                     <h3 className="vertical-timeline-element-title">50 CSS, JS and HTML Mini Projects</h3>
-                    <h4 className="vertical-timeline-element-subtitle">HTML5, JS, CSS</h4>
-                    <p>
-                        dec dec dec
-                    </p>
+                    <h6 className="vertical-timeline-element-subtitle mb-2">HTML5, JS, CSS</h6>
+                    <li>Utilized DOM manipulation, events, array methods, HTTP requests & more to conditionally render content based on user interactions</li>
+                    <li>Developed custom CSS stylesheets to uniquely display content that is adaptive to user interactions</li>
+                    <div className='mt-2'>
+                        <button><a href="https://github.com/MBaiamonte/50_css_js_mini_projects">Git</a></button>
+                    </div>
                 </VerticalTimelineElement>
             </VerticalTimeline>
         </div>
