c52473fcef4394eaf9eadffb534297e40181d589
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-03-06 12:27:41 -0700

    create about me view page comp

---
+++ client/src/views/AboutMePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from '../components/Header';
+import Separator from '../components/Separator';
+import AboutMe from '../components/AboutMe';
+import Footer from '../components/Footer';
+
+const AboutMePage = () => {
+    return (
+        <>
+            <Header/>
+            <Separator/>
+            <AboutMe/>
+            <Footer/>
+        
+        </>
+    )
+}
+
+export default AboutMePage
