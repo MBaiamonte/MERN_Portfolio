2897817f1b2865e398d2b36798a592a0314ad82d
MBaiamonte <120220332+MBaiamonte@users.noreply.github.com>
2024-03-07 17:06:58 -0700

    add scroll animation

--- client/src/views/ProjectsPage.jsx
+++ client/src/views/ProjectsPage.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import Projects from '../components/Projects'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Separator from '../components/Separator'
 
 const ProjectsPage = () => {
+    useEffect(() => {
+        AOS.init({
+            duration: 1000
+        });
+        AOS.refresh();
+    }, []);
     return (
         <>
             <Header/>
             <Separator/>
             <Projects/>
-            <Footer/>
+            <div  data-aos="flip-up">
+                <Footer/>
+            </div>
         </>
     )
 }
